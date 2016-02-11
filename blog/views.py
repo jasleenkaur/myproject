22d49d7ea6e4e3acd5152b6ecd31dd043c237cea
jasleenkaur <jasleen.7956@gmail.com>
2016-02-11 19:49:45 +0530

    blog: index posts added

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Post
 
 def index(request):
+    p = Post.objects.all()
     template = loader.get_template('blog/index.html')
     context = {
-        
+        'posts': p,
     }
     
     return HttpResponse(template.render(context, request))
