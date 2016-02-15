26333747bbabe3de6782dc2c3233c6564c692654
jasleenkaur <jasleen.7956@gmail.com>
2016-02-16 00:57:32 +0530

    tweeking admin site

---
+++ polls/migrations/0006_auto_20160216_0027.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0005_auto_20160214_0955'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='pub_date',
+            field=models.DateTimeField(verbose_name='date published'),
+        ),
+    ]
